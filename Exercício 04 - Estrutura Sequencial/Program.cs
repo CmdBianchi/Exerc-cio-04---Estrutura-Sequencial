2c7230b5a4592361acdda146549e7ef38c0bca39
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-21 11:24:27 -0300

    ADD FUNCTION ( Salary ).

--- Exercício 04 - Estrutura Sequencial/Program.cs
+++ Exercício 04 - Estrutura Sequencial/Program.cs
@@ -8,19 +8,34 @@ namespace Exercício_04___Estrutura_Sequencial
         /////////////////////////////////////////////////////////////////////////////////////////////
         static void Main(string[] args)
         {
-           
+            Msg();
+            Id();
         }
         /// -------> FUNCTIONS
         /////////////////////////////////////////////////////////////////////////////////////////////
         static void Msg() {
             Console.WriteLine("Mostre o número e o salário do funcionário, com duas casas decimais."); 
         }
+        /*------------------------------------------------------------------------------------------*/
         static int Id() {
             int numero;
             Console.Write("Número de série do funcionário: ");
             numero = int.Parse(Console.ReadLine());
             return numero;
         }
+        /*------------------------------------------------------------------------------------------*/
+        static void Salary() {
+            double hours, salary;
+
+            Console.Write("Difite a quantede de horas trabalhadas: ");
+            hours = double.Parse(Console.ReadLine());
+            
+
+
+
+        }
+
+
         /////////////////////////////////////////////////////////////////////////////////////////////
         /// -------> END
     }
