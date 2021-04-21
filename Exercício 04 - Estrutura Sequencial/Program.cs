7c0b6f5bc99a168efeaf9cae40f7e933f50ce379
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-21 11:03:33 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício 04 - Estrutura Sequencial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercício_04___Estrutura_Sequencial
+{
+    class Program
+    {
+        /// -------> START / MAIN 
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args)
+        {
+           
+        }
+        /// -------> FUNCTIONS
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Msg() {
+            Console.WriteLine("Mostre o número e o salário do funcionário, com duas casas decimais."); 
+        }
+        static void Id() {
+            int numero;
+            Console.Write("Número de série do funcionário: ");
+            numero = int.Parse(Console.ReadLine());
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        /// -------> END
+    }
+}
